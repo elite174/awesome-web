dac21fc2e710dd0a85a81aa90a52fef3b2742b5b
Vladislav <lipatov.xex@gmail.com>
2020-08-30 20:41:34 +0300

    in progress

---
+++ data/index.ts
@@ -0,0 +1,8 @@
+export interface Link {
+    readonly title: string;
+    readonly description: string;
+    readonly url: string;
+    readonly tags: string[];
+}
+
+export const links: Link[] = [];
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,39 @@
-import styled from '@emotion/styled';
+import React, { FC } from 'react';
+import { css } from 'emotion';
 
-const Title = styled.h1`
-    color: red;
-    font-size: 50px;
+import { links, Link } from '../data';
+
+const pageStyles = css`
+    min-height: 100vh;
 `;
 
+const titleStyles = css``;
+
+const linksContainerStyles = css``;
+
+const linkStyles = css``;
+
+const linkContainerStyles = css``;
+
+interface Props {
+    readonly link: Link;
+}
+
+const Link: FC<Props> = ({ link }) => (
+    <div className={linkContainerStyles}>
+        <a className={linkStyles}></a>;
+    </div>
+);
+
 export default function Home() {
-    return <Title>My page</Title>;
+    return (
+        <div className={pageStyles}>
+            <div className={titleStyles}>Awesome web</div>
+            <div className={linksContainerStyles}>
+                {links.map((l) => (
+                    <Link key={l.url} link={l} />
+                ))}
+            </div>
+        </div>
+    );
 }
